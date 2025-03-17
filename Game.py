634bc16777afabe61ae6dd3ec9e06f58faf296a1
JosalbertoTizon <josalberto314@gmail.com>
2025-03-17 01:27:11 -0300

    Initial Commit: Basic files needed to run a black game window

---
+++ Game.py
@@ -0,0 +1,22 @@
+import pygame
+from settings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+
