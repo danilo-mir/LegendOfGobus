bea5b5d7a9fd83b50af588d45eecfd666b1fa9d1
JosalbertoTizon <josalberto314@gmail.com>
2025-03-17 02:15:25 -0300

    Feat: Add interface for game objects like player and obstacles, and add a debug file

--- Game.py
+++ Game.py
@@ -1,11 +1,16 @@
 import pygame
 from settings import *
+from debug import debug
+from level import Level
 
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('The Legend of Goobus: Breath of the Baphen')
         self.clock = pygame.time.Clock()
+
+        self.level = Level()
 
     def run(self):
         while True:
@@ -15,6 +20,7 @@ class Game:
                     quit()
 
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
---
+++ level.py
@@ -0,0 +1,28 @@
+import pygame
+from settings import *
+from tile import Tile
+from player import Player
+
+class Level:
+  def __init__(self):
+    # Acesso à variável da tela
+    self.display_surface = pygame.display.get_surface()
+    
+    # Criar grupos de sprites
+    self.visibile_sprites = pygame.sprite.Group()
+    self.obstacle_sprites = pygame.sprite.Group()
+
+    # Criar sprite do mapa
+    self.create_map()
+
+  def create_map(self):
+    for row_index, row in enumerate(WORLD_MAP):
+      for col_index, col in enumerate(row):
+        x = col_index * TILESIZE
+        y = row_index * TILESIZE
+        if col == 'R':
+          Tile((x, y), [self.visibile_sprites])
+
+
+  def run(self):
+    self.visibile_sprites.draw(self.display_surface)
