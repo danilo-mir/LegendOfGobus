2cec6bd6b6bc123540ba64f7c43d7555a7ce2450
AugustoNasc <augustonascimentodeoliveira30@gmail.com>
2025-04-03 21:07:44 -0300

    movimentacao_jogador

--- codigo/nivel.py
+++ codigo/nivel.py
@@ -22,7 +22,7 @@ class Nivel:
                 if col == 'x':
                     Quadrado((x, y), [self.visible_sprites, self.obstacle_sprites])
                 if col == 'p':
-                    self.player = Jogador((x, y), [self.visible_sprites])
+                    self.player = Jogador((x, y), [self.visible_sprites], self.obstacle_sprites)
     def run(self):
         self.visible_sprites.draw(self.display_surface)
         self.visible_sprites.update()
