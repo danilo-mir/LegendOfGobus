c04f510817b822cdb92d01c4c26a919bc452e689
Geison <geison.filho@evtit.com>
2025-04-16 08:56:48 -0300

    Change super

--- player.py
+++ player.py
@@ -200,7 +200,7 @@ class Player(Entity):
             if keys[pygame.K_r]:
                 if self.super_counter >= self.player_stats['super_threshold']:
                     self.super_counter = 0
-                    self.health = min(self.player_stats['max_health'], self.health*1.5)
+                    self.health = min(self.player_stats['max_health'], self.health + 125)
             if keys[pygame.K_m]:
                 if self.super_counter < self.player_stats['super_threshold']:
                     self.super_counter += 1
